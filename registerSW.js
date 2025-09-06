9cd32e577e38e75d8191b08ae77450e7d0611528
Piyush Mishra <piyushmishra@Piyushs-MacBook-Air.local>
2025-09-06 16:44:54 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/staruniversalgroup/sw.js', { scope: '/staruniversalgroup/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
