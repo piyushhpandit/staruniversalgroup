a0274e22a0faa812c7ff91f67b2994d304a312e2
Piyush Mishra <mpiyush243@gmail.com>
2025-12-13 10:42:00 +0530

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api", // backend base URL
+  baseURL: "https://staruniversalgroup-be.onrender.com/api" || "http://localhost:4000/api", // backend base URL
   // baseURL: "http://localhost:4000/api", // backend base URL
   headers: {
     "Content-Type": "application/json",
